cf2f78461d0a2cfb1827064eb94fbcb04d5a6a4e
shishir-chowdhury <shiishir@gmail.com>
2025-08-22 17:04:09 +0600

    add AdminController

---
+++ app/Http/Controllers/Api/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Book;
+
+class AdminController extends Controller
+{
+    public function users()
+    {
+        $users = User::select('id','name','email')->get();
+        return response()->json(['users' => $users]);
+    }
+
+    public function books()
+    {
+        $books = Book::with('user:id,name')->get()->map(function ($b) {
+            return [
+                'id'     => $b->id,
+                'title'  => $b->title,
+                'author' => $b->author,
+                'user'   => ['id' => $b->user->id, 'name' => $b->user->name],
+            ];
+        });
+
+        return response()->json(['books' => $books]);
+    }
+
+    public function deleteBook($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+        return response()->json(['message' => 'Book deleted successfully']);
+    }
+}
