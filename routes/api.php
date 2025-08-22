11f8f5ce9a9c090dc876920d7d073091126883f6
shishir-chowdhury <shiishir@gmail.com>
2025-08-22 20:07:01 +0600

    modify the routes for solve bugs

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\AdminController;
 
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login',    [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:api'])->group(function () {
+Route::group([], function () {
     Route::get('me', [AuthController::class, 'me']);
 
-    Route::post('books',        [BookController::class, 'store']);
-    Route::get('books/nearby',  [BookController::class, 'nearby']);
+    Route::post('books', [BookController::class, 'store']);
+    Route::get('books/nearby', [BookController::class, 'nearby']);
 });
 
-Route::prefix('admin')->middleware(['auth:api','is_admin'])->group(function () {
-    Route::get('users',          [AdminController::class, 'users']);
-    Route::get('books',          [AdminController::class, 'books']);
-    Route::delete('books/{id}',  [AdminController::class, 'deleteBook']);
+Route::prefix('admin')->group(function () {
+    Route::get('users', [AdminController::class, 'users']);
+    Route::get('books', [AdminController::class, 'books']);
+    Route::delete('books/{id}', [AdminController::class, 'deleteBook']);
 });
